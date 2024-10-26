7e60f25b1eedf5c7885330c39bac4bbd0f78734d
Erkam Boyacioglu <boyaciogluerkam@gmail.com>
2024-10-26 13:26:48 -0400

    Initial commit for DisplayTTC

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <vector>
+
+
+
+
+
+// put declatations here
+
+//struct for stops
+struct stopLocationStruct
+{
+String Name;
+int stopid;
+int LEDindex; //which line of leds 1 = line1, 2 = 
+};
+
+std::vector<stopLocationStruct> = {
+  {"Finch Station", 14111, 1},
+  {"Finch as"},
+  {},
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
