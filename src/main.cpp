4658df6e523b67a612be24e39d05e91a6b408616
Erkam Boyacioglu <boyaciogluerkam@gmail.com>
2024-10-26 22:34:05 -0400

    HTTP connection and testing GET commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,36 @@
 
 
 // put declatations here
+const char* ssid = "Banana Guest";
+const char* password = "Banana1245"; //wifi settings
+
+const char* server = "ntas.ttc.ca";
+WiFiClientSecure client;
+//https certificiate to server
+ const char *certificate =      "-----BEGIN CERTIFICATE-----\n"
+                                "MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G\n" 
+                                "A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp\n" 
+                                "Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4\n" 
+                                "MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG\n" 
+                                "A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n" 
+                                "hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8\n" 
+                                "RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT\n"
+                                "gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm\n"
+                                "KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd\n"
+                                "QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ\n"
+                                "XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw\n"
+                                "DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o\n"
+                                "LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU\n"
+                                "RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp\n"
+                                "jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK\n"
+                                "6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX\n"
+                                "mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs\n"
+                                "Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH\n"
+                                "WD9f\n"
+                                "-----END CERTIFICATE-----\n";
+
+
+
 
 
 //struct for stops
@@ -94,6 +124,7 @@ std::vector<stopLocationStruct> greenLineStopInstructions= {
   {14947,"Kennedy Station - Subway Platform",71},
 };
 
+
 //Bindings for Purple
 std::vector<stopLocationStruct> purpleStopLocations
 {
@@ -104,14 +135,75 @@ std::vector<stopLocationStruct> purpleStopLocations
   {14949,"Don Mills Station - Subway Platform",76},
 };
 
-
+int samplestops[3] = {13757,13742,14947};
 
 void setup() {
   // put your setup code here, to run once:
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print(".");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Attempting Connection to Wifi network ..");
+
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // wait 1 second for re-trying
+    delay(1000);
+  }
+
+  Serial.println(WiFi.localIP());
+
+}
+
+
+
+
+
+void apiconnection(const char* servername,int stopID){
+  String output;
+
+
+    client.setCACert(certificate); //assign our root certificate to the client.
+    Serial.printf("[HTTPS] GET request begins\n");
+    if(!client.connect(servername,443) ){ //if connection cannot be achieved
+        Serial.printf("[HTTPS] Get has failed...");
+    }
+    else{
+    Serial.println("Connected to server!");
+    //make HTTPS request;
+    String serverLink = "GET /api/ntas/get-next-train-time/" + String(stopID) + " HTTP/1.1";
+    client.println(serverLink);
+    client.println("Host: ntas.ttc.ca");
+    client.println("Connection: close");
+    client.println();
+
+    String response = client.readString();
+    Serial.println("Response:");
+    Serial.print(response);
+    client.stop();
+
+    }
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Start GET request");
+  apiconnection(server,13852);
+  Serial.println("DONE");
+  delay(10000);
+
+  int i = 0;
+  while(i < 3){
+    apiconnection(server,samplestops[i]);
+    i++;
+  }
 }
 
 // put function definitions here:
+
